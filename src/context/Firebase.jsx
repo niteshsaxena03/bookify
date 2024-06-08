2ce9783b61d0c44c8f53d6534c6d9dfd57f35bcb
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-06-08 09:38:31 +0530

    Some other changes

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -1,9 +1,20 @@
 import { createContext, useContext } from "react";
+import { initializeApp } from "firebase/app";
+const FirebaseContext = createContext(null);
 
-const Firebase = createContext(null);
+const firebaseConfig = {
+  apiKey: "AIzaSyChLIgTS48izN7zArySRh5fIFYKBehKNYU",
+  authDomain: "bookify-ab720.firebaseapp.com",
+  projectId: "bookify-ab720",
+  storageBucket: "bookify-ab720.appspot.com",
+  messagingSenderId: "209630862734",
+  appId: "1:209630862734:web:23340ccc82bdee0fb918df",
+};
 
-export const FirebaseContext = useContext(Firebase);
+export const firebaseApp = initializeApp(firebaseConfig);
 
-export const FirebaseProvider = (props) => {
-  return <FirebaseContext.Provider>{props.children}</FirebaseContext.Provider>;
+export const useFirebase = () => useContext(FirebaseContext);
+
+export const FirebaseProvider = ({children}) => {
+  return <FirebaseContext.Provider value={FirebaseContext}>{children}</FirebaseContext.Provider>;
 };
